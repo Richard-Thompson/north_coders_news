c30c4cb4be7e096d2214cca8ca2452947f0ee066
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-05-17 10:51:46 +0100

    Testing comments reducer

---
+++ spec/comments.reducer.spec.js
@@ -0,0 +1,159 @@
+/* eslint-env node, mocha, chai */
+import {expect} from 'chai';
+import commentsReducer from '../src/reducer/comments.reducer';
+import * as actions from '../src/actions/actions';
+
+describe('comments.reducer', () => {
+  it('handles FETCH_COMMENTS_REQUESTS', () => {
+    const action = actions.fetchCommentsRequest();
+    const initialState = {
+      loading: false
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: true
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles FETCH_COMMENTS_SUCCESS', () => {
+    const action = actions.fetchCommentsSuccess('comment');
+    const initialState = {
+      loading: true,
+      comments: []
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      comments:['comment']
+    };
+    expect(actual).to.eql(expected);
+    expect(expected.comments).to.not.equal(initialState.comments);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles FETCH_COMMENTS_ERROR', () => {
+    const action = actions.fetchCommentsError('error');
+    const initialState = {
+      loading: true,
+      error: null
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      error: 'error'
+    };
+    expect(actual).to.eql(expected);
+  });
+
+  it('handles VOTE_COMMENT_REQUESTS', () => {
+    const action = actions.commentVoteRequest();
+    const initialState = {
+      loading: false
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: true
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles VOTE_COMMENT_SUCCESS UP', () => {
+    const action = actions.commentVoteSuccess(1, '?vote=up');
+    const initialState = {
+      loading: true,
+      comments: [{_id:1, votes: 2}]
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      comments: [{_id:1, votes: 3}]
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles VOTE_COMMENT_SUCCESS DOWN', () => {
+    const action = actions.commentVoteSuccess(1, '?vote=down');
+    const initialState = {
+      loading: true,
+      comments:  [{_id:1, votes: 3}]
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      comments:  [{_id:1, votes: 2}]
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles VOTE_COMMENT_ERROR', () => {
+    const action = actions.commentVoteError('error');
+    const initialState = {
+      loading: true,
+      error: null
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      error: 'error'
+    };
+    expect(actual).to.eql(expected);
+  });
+
+  it('handles ADD_COMMENT_REQUESTS', () => {
+    const action = actions.addCommentRequest();
+    const initialState = {
+      loading: false
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: true
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles ADD_COMMENT_SUCCESS', () => {
+    const action = actions.addCommentSuccess({_id: 2, comment: 'test'});
+    const initialState = {
+      loading: true,
+      comments: [{_id: 1, comment: 'hello'}]
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      comments: [{_id: 1, comment: 'hello'}, {_id: 2, comment: 'test'}]
+    };
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(initialState);
+  });
+
+  it('handles ADD_COMMENT_ERROR', () => {
+    const action = actions.addCommentError('error');
+    const initialState = {
+      loading: true,
+      error: null
+    };
+
+    const actual = commentsReducer(initialState, action);
+    const expected = {
+      loading: false,
+      error: 'error'
+    };
+    expect(actual).to.eql(expected);
+  });
+
+});
