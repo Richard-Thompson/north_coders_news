b992fdfb8f6bbf640da45dbee0b5c0a1a01964e0
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-05-30 16:14:52 +0100

    added a production config file with node.env

---
+++ webpack.prod.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'js'),
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
